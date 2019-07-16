ebb169afb3e8261d7e68ff6fca0682da6d2bc3d3
zyzyzyk <1805250680@qq.com>
2019-07-16 17:07:37 +0800

    zyk

--- React/redux-counter/src/App.js
+++ React/redux-counter/src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {connect} from 'react-redux'
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Count extends Component {
+  state = {  }
+  handleIn = ( ) => {
+    // store.dispatch({ type: 'INCREMENT' })
+    this.props.increment();
+  }
+  handleDe = () => {
+    // store.dispatch({ type: 'DECREMENT' })
+    this.props.decrement();
+  }
+  handleAddFilm = () => {
+    // store.dispatch(
+    //   {
+    //     type: 'ADD_FILM',  
+    //     film: {name: '蜘蛛侠'}
+    //   })
+    this.props.add_film();
+  }
+  render() { 
+    console.log(this.props);
+    const { count, films } = this.props
+    return ( 
+      <div>
+        count: {count}
+        <button onClick={this.handleIn}>+</button>
+        <button onClick={this.handleDe}>-</button>
+        <button onClick={this.handleAddFilm}>添加电影</button>
+        {
+          films.map((film, i) => {
+            return (
+              <li key={i}>{film.name}</li>
+            )
+          })
+        }
+      </div>
+    );
+  }
 }
-
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    count: state.count,
+    films: state.films
+  }
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    foo: () => {console.log('aaa')},
+    increment: () => {dispatch({type: 'INCREMENT'})},
+    decrement: () => {dispatch({type: 'DECREMENT'})},
+    add_film: () =>{
+      dispatch({
+        type: 'ADD_FILM',
+        film: {name: '蜘蛛侠'}
+      })
+    }
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Count);
--- my-app/src/api/movie.ts
+++
@@ -1,6 +0,0 @@
-export const getMovieList = (cate: string) => {
-  return request({
-    url: `/api/movie/${cate}?apikey=0df993c66c0c636e29ecbb5344252a4a,
-    methods: 'GET'
-  })
-}
