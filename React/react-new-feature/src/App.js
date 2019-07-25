6a9f5577c24694cf96ac3b9e3c5fd753f32d91f5
zyzyzyk <1805250680@qq.com>
2019-07-25 11:27:13 +0800

    zyk

---
+++ React/react-new-feature/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import ConcurrentModeDemo from './concurrent/index'
+
+function App() {
+  return (
+   <div>
+     <ConcurrentModeDemo />
+   </div> 
+  )
+}
+
+export default App;
