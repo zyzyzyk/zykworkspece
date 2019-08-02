cfcc880d875460cb9f9e740eab0b9ee61649d095
zyzyzyk <1805250680@qq.com>
2019-08-02 17:03:48 +0800

    zyk

--- typescript/type/main.js
+++ typescript/type/main.js
@@ -6,3 +6,11 @@ var user_name = "nob";
 var sentence = "Hello,my name is " + name;
 var list = [1, 2, 3];
 var list2 = [1, 2, 3];
+function div(x) {
+    if (isFinite(1000 / x)) {
+        return 'Number is NOT Infinity';
+    }
+    ;
+    return 'Number is Infinity';
+}
+console.log(div(0));
--- typescript/type/main.ts
+++ typescript/type/main.ts
@@ -8,3 +8,11 @@ let sentence: string = `Hello,my name is ${name}`;
 
 let list: number[] = [1, 2, 3];
 let list2: Array<number> = [1,2,3];
+
+function div(x) {
+  if(isFinite(1000 / x)){
+    return 'Number is NOT Infinity'
+  };
+  return 'Number is Infinity'
+}
+console.log(div(0))
