b58e711beb8ca1919d95f20ef2f39a0a3aa1dc70
zyzyzyk <1805250680@qq.com>
2019-08-08 16:08:48 +0800

    sleep

--- kuaishou/sleep/index.js
+++ kuaishou/sleep/index.js
@@ -5,3 +5,39 @@
 //   .then(() => {
 //     console.log('1');
 //   })
+
+// function *sleepGenerator(time) {
+//   yield new Promise((resolve, reject) => {
+//     setTimeout(resolve, time);
+//   });// 碰到yield停下来
+//   console.log('aaaa');
+// }
+// sleepGenerator(0)
+//   .next()
+//   .value
+//   .then(() => {
+//     console.log('lll');
+//   })
+// console.log(sleepGenerator().next().value.then(() => {
+//   console.log('aaa');
+// }));
+// console.log(sleepGenerator().next());
+
+// function sleep(time) {
+//   return new Promise(resolve => setTimeout(resolve, time));
+// }
+// async function output() {
+//   let out = await sleep(1000);
+//   console.log(1);
+//   return out;
+// }
+// output();
+
+function sleep(callback, time) {
+  if(typeof callback === 'function') {
+    setTimeout(callback, time);
+  }
+}
+sleep(function() {
+  console.log('sss');
+},1000)
