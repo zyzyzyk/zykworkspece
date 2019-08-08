d9add63661534377e3c28ea3e2ae2c5fa801c2fc
zyzyzyk <1805250680@qq.com>
2019-08-08 15:30:48 +0800

    快手面试

--- kuaishou/algorithm/getMax.js
+++ kuaishou/algorithm/getMax.js
@@ -1,6 +1,24 @@
 var str = 'aaaabbbeeeghghghsakk';
 // 出现次数最多 a
 function getMax() {
-  let hash = {} // hash表
+  let hash = {}, // hash表
+    max = 0, maxstr;
+  for(let i = 0; i < str.length; i++){
+    // hash有i吗
+    // 有加1
+    // 没有 1
+    let char = str[i];
+    if(!hash[char]) {
+      // 没有
+      hash[char] = 1;
+    }else {
+      hash[char]++;
+    }
+    if(hash[char] > max) {
+      max = hash[char];
+      maxstr = char;
+    }
+  }
+  return maxstr;
 }
 console.log(getMax(str));
---
+++ kuaishou/sleep/index.js
@@ -0,0 +1,7 @@
+// const sleep = time => {
+//   return new Promise((resolve, reject) => setTimeout(resolve, time))
+// }
+// sleep(5000)
+//   .then(() => {
+//     console.log('1');
+//   })
